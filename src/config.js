72409c5e5c93e5e7c9302e1c26dae140497c97da
Max Sender <msender3489@gmail.com>
2021-07-28 17:57:46 -0500

    port and site

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ var config = {};
 config.socketIO = {};
 config.npmClient ={};
 
-config.socketIO.port = 4001;
-config.npmClient.siteName = "192.168.1.239";
+config.socketIO.port = 5000;
+config.npmClient.siteName = "https://step-to-the-line-backend.herokuapp.com";
 
 module.exports = config;
